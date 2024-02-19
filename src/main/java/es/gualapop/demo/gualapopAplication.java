81134c9cbd00efc72d89c35993d45d1113d9f6e0
cassiel123 <45970422+Cassiel123@users.noreply.github.com>
2024-02-19 11:42:45 +0100

    insertado proyecto spring boot

---
+++ src/main/java/es/gualapop/demo/gualapopAplication.java
@@ -0,0 +1,13 @@
+package es.gualapop.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class gualapopAplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(gualapopAplication.class, args);
+    }
+
+}
