cd18f3fa722db2ad2afd8e63122a7126daa35323
cassiel123 <45970422+Cassiel123@users.noreply.github.com>
2024-02-20 10:44:25 +0100

    model product

---
+++ backend/src/main/java/es/gualapop/backend/model/Product.java
@@ -0,0 +1,77 @@
+package es.gualapop.backend.model;
+
+import jakarta.persistence.*;
+
+import java.sql.Blob;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id = null;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Lob
+    private Blob imageFile;
+
+    private boolean image;
+
+
+    public Product() {}
+
+    public Product(String nombre, String description) {
+        super();
+        this.title = nombre;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Blob getImageFile() {
+        return imageFile;
+    }
+
+    public void setImageFile(Blob image) {
+        this.imageFile = image;
+    }
+
+    public boolean getImage(){
+        return this.image;
+    }
+
+    public void setImage(boolean image){
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [id=" + id + ", title=" + title + ", description=" + description + "]";
+    }
+}
