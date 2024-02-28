3f5250d9313ccfc9ca538447669b7fa1c5984136
n3kr00Gl00m <=>
2024-02-28 17:43:47 +0100

    Cambios en index controllers

--- backend/src/main/java/es/gualapop/backend/controller/NavigationController.java
+++ backend/src/main/java/es/gualapop/backend/controller/NavigationController.java
@@ -8,12 +8,14 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import es.gualapop.backend.service.LoaderService;
 
+@Controller
 public class NavigationController {
 
     @Autowired
