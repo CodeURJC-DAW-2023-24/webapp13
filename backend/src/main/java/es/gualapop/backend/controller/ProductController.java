935cee2e4c488d2dd199a1ed1b00ebc7c58bcf99
Cassiel123 <4610@alumnoaltamira.es>
2024-03-01 19:27:03 +0100

    new product al 90%

--- backend/src/main/java/es/gualapop/backend/controller/ProductController.java
+++ backend/src/main/java/es/gualapop/backend/controller/ProductController.java
@@ -7,6 +7,7 @@ import es.gualapop.backend.repository.ProductTypeRepository;
 import es.gualapop.backend.repository.UserRepository;
 import es.gualapop.backend.service.ProductService;
 import es.gualapop.backend.service.SearchService;
+import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
@@ -22,10 +23,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Optional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @Controller
@@ -151,7 +154,66 @@ public class ProductController {
 
         return "redirect:/";
     }
-    
 
+    @GetMapping("/newProduct")
+    public String newProducts(Model model) {
+
+        return "newProduct";
+    }
+
+
+    @PostMapping("/addNewProduct")
+    public String addNewProduct(Model model,
+                                @RequestParam("Title") String title,
+                                @RequestParam("Description") String description,
+                                @RequestParam("Category") String category,
+                                @RequestParam("Address") String Address,
+                                @RequestParam("City") String City,
+                                @RequestParam("Province") String Province,
+                                @RequestParam("imageFile") MultipartFile imageFile,
+                                @RequestParam("Cp") String Cp) throws IOException {
+        //crear nuevo objeto product y settear atributows
+
+        Product product = new Product();
+        product.setTitle(title);
+        product.setDescription(description);
+        switch (category) {
+            case "Electrónica":
+                product.setProductType(1L);
+                break;
+            case "Muebles":
+                product.setProductType(2L);
+                break;
+            case "Ropa":
+                product.setProductType(3L);
+                break;
+            case "Libros":
+                product.setProductType(4L);
+                break;
+            case "Deportes":
+                product.setProductType(5L);
+                break;
+            case "Hogar y Jardín":
+                product.setProductType(6L);
+                break;
+            case "Juguetes":
+                product.setProductType(7L);
+                break;
+            default:
+                // Manejar cualquier otro caso si es necesario
+                break;
+        }
+        String fullAddress = Address + ", " + City + ", " + Province + ", " + Cp;
+        product.setAddress(fullAddress);
+        if (!imageFile.isEmpty()) {
+            product.setImageFile(BlobProxy.generateProxy(imageFile.getInputStream(), imageFile.getSize()));
+            product.setImage(true);
+        }
+
+        //save con el repository
+        productRepository.save(product);
+
+        return "redirect:/index";
+    }
 
 }
