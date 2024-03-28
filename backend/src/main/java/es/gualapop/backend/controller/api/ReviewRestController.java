f812a14da416320b9d5988bffd71380ff23ba932
Fervo8564 <fernandoprietoolias@gmail.com>
2024-03-28 13:43:08 +0100

    API POST Review to a user

--- backend/src/main/java/es/gualapop/backend/controller/api/ReviewRestController.java
+++ backend/src/main/java/es/gualapop/backend/controller/api/ReviewRestController.java
@@ -2,18 +2,26 @@ package es.gualapop.backend.controller.api;
 
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
+import es.gualapop.backend.model.Report;
 import es.gualapop.backend.model.Review;
+import es.gualapop.backend.model.User;
 import es.gualapop.backend.repository.ReviewRepository;
+import es.gualapop.backend.repository.UserRepository;
 
 @RestController
 @CrossOrigin
@@ -22,6 +30,9 @@ public class ReviewRestController {
 
     @Autowired
     private ReviewRepository reviewRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @JsonView(Review.Detailed.class)
     @GetMapping("/{id}")
@@ -34,5 +45,28 @@ public class ReviewRestController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @JsonView(Review.Detailed.class)
+    @PostMapping("/new")
+    public ResponseEntity<String> postReview(HttpServletRequest request,
+                                               @RequestParam("rating") float rating,
+                                               @RequestParam("sellerID") Long sellerID) {
+        //Create new report and set attributes
+        Review review = new Review();
+        Optional<User> user = userRepository.findById(sellerID);
+        if(!user.isPresent()){
+            return ResponseEntity.badRequest().body("Seller not found");
+        }
+        if(rating<0 || rating>5){
+            return ResponseEntity.badRequest().body("Review's rating must be between 0 and 5");
+        }
+        
+        review.setRating(rating);
+        review.setSellerID(sellerID);
+
+        reviewRepository.save(review);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("Review added successfully");
     }
 }
