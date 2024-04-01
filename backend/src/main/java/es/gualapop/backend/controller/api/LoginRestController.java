d2d9ad2ec5871332d39c1ef5aea8d137e65d5a0a
n3kr00Gl00m <=>
2024-04-02 01:07:00 +0200

    Login method created, fixed some methods of ReportRestController

---
+++ backend/src/main/java/es/gualapop/backend/controller/api/LoginRestController.java
@@ -0,0 +1,38 @@
+package es.gualapop.backend.controller.api;
+
+import es.gualapop.backend.security.jwt.AuthResponse;
+import es.gualapop.backend.security.jwt.LoginRequest;
+import es.gualapop.backend.security.jwt.UserLoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/api/auth")
+public class LoginRestController {
+    @Autowired
+    private UserLoginService userService;
+
+    @PostMapping("/login")
+
+    public ResponseEntity<AuthResponse> login(
+            @CookieValue(name = "accessToken", required = false) String accessToken,
+            @CookieValue(name = "refreshToken", required = false) String refreshToken,
+            @RequestBody LoginRequest loginRequest) {
+        return userService.login(loginRequest, accessToken, refreshToken);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthResponse> refreshToken(
+            @CookieValue(name = "refreshToken", required = false) String refreshToken) {
+        return userService.refresh(refreshToken);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<AuthResponse> logOut(HttpServletRequest request, HttpServletResponse response) {
+        return ResponseEntity.ok(new AuthResponse(AuthResponse.Status.SUCCESS, userService.logout(request, response)));
+    }
+}
--- backend/src/main/java/es/gualapop/backend/security/SecurityConfiguration.java
+++ backend/src/main/java/es/gualapop/backend/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package es.gualapop.backend.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,7 +28,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
-
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
