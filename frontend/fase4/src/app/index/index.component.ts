d458ddbd1e4d049ead60e9930ee8413d810d744e
Romofe01 <romofe01@gmail.com>
2024-06-11 21:58:23 +0200

    Fix Categories + Products

--- frontend/fase4/src/app/index/index.component.ts
+++ frontend/fase4/src/app/index/index.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductType } from '../Models/product-type.model';
+import { Product } from '../Models/product.model';
 import { ProductService } from '../Services/product.service';
 
 @Component({
@@ -8,9 +8,37 @@ import { ProductService } from '../Services/product.service';
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent implements OnInit {
+  products: Product[] = [];
+  page: number = 1;
+  size: number = 8;
+  productType: number = 0;
 
-  constructor() {}
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
+    this.productService.getAllProducts(this.page, this.size, this.productType).subscribe(
+      (data: Product[]) => {
+        this.products = data;
+      },
+      error => {
+        console.error('Error fetching products:', error);
+      }
+    );
+  }
+
+  loadMoreProducts(): void {
+    this.page++;
+    this.productService.getAllProducts(this.page, this.size, this.productType).subscribe(
+      (data: Product[]) => {
+        this.products = this.products.concat(data);
+      },
+      error => {
+        console.error('Error fetching more products:', error);
+      }
+    );
   }
 }
